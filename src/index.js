1651b6a5d8e23b5af88cdddb7952f8baa4203297
harcanjo <hfarcanjo@gmail.com>
2022-05-21 17:36:53 -0300

    testes na função hexadecimalParaDecimal

--- src/index.js
+++ src/index.js
@@ -1,90 +1,97 @@
 const mapToDec = {
-	a: 10,
-	b: 11,
-	c: 12,
-	d: 13,
-	e: 14,
-	f: 15,
-};
+  a: 10,
+  b: 11,
+  c: 12,
+  d: 13,
+  e: 14,
+  f: 15,
+}
 
 const mapToHex = {
-	10: 'a',
-	11: 'b',
-	12: 'c',
-	13: 'd',
-	14: 'e',
-	15: 'f',
-};
+  10: 'a',
+  11: 'b',
+  12: 'c',
+  13: 'd',
+  14: 'e',
+  15: 'f',
+}
 
 export function decimalParaHexadecimal(dec) {
-	const valoresInvalidos = ['', undefined, null];
-	const valorEhDiferenteNumero = typeof dec !== 'number';
-	const valorInvalidoDeRGB = dec < 0 || dec > 255;
-	const valorEhFloat = String(dec).includes('.');
+  const valoresInvalidos = ['', undefined, null]
+  const valorEhDiferenteNumero = typeof dec !== 'number'
+  const valorInvalidoDeRGB = dec < 0 || dec > 255
+  const valorEhFloat = String(dec).includes('.')
 
-	if (valorEhDiferenteNumero || valorInvalidoDeRGB || valorEhFloat)
-		return 'Isso não é um valor válido';
+  if (valorEhDiferenteNumero || valorInvalidoDeRGB || valorEhFloat)
+    return 'Isso não é um valor válido'
 
-	const result = dec / 16;
+  const result = dec / 16
 
-	const ehInteiro = !String(result).includes('.');
+  const ehInteiro = !String(result).includes('.')
 
-	if (ehInteiro && result != dec) {
-		const stringDec = String(result);
-		if (result < 10) return stringDec + 0;
-		// if (result % 16 == 0) =
-		return mapToHex[stringDec] + 0;
-	}
+  if (ehInteiro && result != dec) {
+    const stringDec = String(result)
+    if (result < 10) return stringDec + 0
+    // if (result % 16 == 0) =
+    return mapToHex[stringDec] + 0
+  }
 
-	const [inteiro, otherdecimal] = String(result).split('.');
-	const decimal = Number('0.' + otherdecimal);
+  const [inteiro, otherdecimal] = String(result).split('.')
+  const decimal = Number('0.' + otherdecimal)
 
-	const primeiroHex = Number(inteiro) < 10 ? inteiro : mapToHex[inteiro];
-	const preSegundoHex = String(decimal * 16);
-	const segundoHex = Number(preSegundoHex) < 10 ? preSegundoHex : mapToHex[preSegundoHex];
+  const primeiroHex = Number(inteiro) < 10 ? inteiro : mapToHex[inteiro]
+  const preSegundoHex = String(decimal * 16)
+  const segundoHex =
+    Number(preSegundoHex) < 10 ? preSegundoHex : mapToHex[preSegundoHex]
 
-	return primeiroHex + segundoHex;
+  return primeiroHex + segundoHex
 }
 
 export function rgbParaHexadecimal(red, green, blue) {
-	if ( (decimalParaHexadecimal(red)) === 'Isso não é um valor válido') return { erro: decimalParaHexadecimal(red) }
-	if ( (decimalParaHexadecimal(green)) === 'Isso não é um valor válido') return { erro: decimalParaHexadecimal(green) }
-	if ( (decimalParaHexadecimal(blue)) === 'Isso não é um valor válido') return { erro: decimalParaHexadecimal(blue) }
-	return {
-		red: decimalParaHexadecimal(red),
-		green: decimalParaHexadecimal(green),
-		blue: decimalParaHexadecimal(blue),
-	};
+  if (decimalParaHexadecimal(red) === 'Isso não é um valor válido')
+    return { erro: decimalParaHexadecimal(red) }
+  if (decimalParaHexadecimal(green) === 'Isso não é um valor válido')
+    return { erro: decimalParaHexadecimal(green) }
+  if (decimalParaHexadecimal(blue) === 'Isso não é um valor válido')
+    return { erro: decimalParaHexadecimal(blue) }
+  return {
+    red: decimalParaHexadecimal(red),
+    green: decimalParaHexadecimal(green),
+    blue: decimalParaHexadecimal(blue),
+  }
 }
 
 export function hexadecimalParaDecimal(hexadecimal) {
-	const valoresInvalidos = ['', undefined, null];
-	if (
-		hexadecimal.length > 2 ||
-		String(hexadecimal).includes('-') ||
-		valoresInvalidos.includes(hexadecimal)
-	)
-		return 'Isso não é um valor válido';
-	const hex = String(hexadecimal).toLowerCase();
-	const letras = ['a', 'b', 'c', 'd', 'e', 'f'];
-	const [primeiroHex, segundoHex] = String(hex);
-	const ehUmaLetra = (value) => letras.includes(value);
-	const prePrimeiroHex = ehUmaLetra(primeiroHex)
-		? Number(mapToDec[primeiroHex])
-		: Number(primeiroHex);
-	const primeiroDec = prePrimeiroHex * 16;
+  const valoresInvalidos = ['', undefined, null, '.', '-']
+  if (
+    hexadecimal.length > 2 ||
+    typeof hexadecimal !== 'string' ||
+    valoresInvalidos.includes(hexadecimal) ||
+    hexadecimal.search(/\W/gi) !== -1
+  )
+    return 'Isso não é um valor válido'
+  const hex = String(hexadecimal).toLowerCase()
+  const letras = ['a', 'b', 'c', 'd', 'e', 'f']
+  const [primeiroHex, segundoHex] = String(hex)
+  const ehUmaLetra = value => letras.includes(value)
+  const prePrimeiroHex = ehUmaLetra(primeiroHex)
+    ? Number(mapToDec[primeiroHex])
+    : Number(primeiroHex)
+  const primeiroDec = prePrimeiroHex * 16
 
-	const segundoDec = ehUmaLetra(segundoHex) ? Number(mapToDec[segundoHex]) : Number(segundoHex);
+  const segundoDec = ehUmaLetra(segundoHex)
+    ? Number(mapToDec[segundoHex])
+    : Number(segundoHex)
 
-	return String(primeiroDec + segundoDec);
+  return String(primeiroDec + segundoDec)
 }
 
 export function hexadecimalParaRGB(red, green, blue) {
-	return {
-		red: hexadecimalParaDecimal(red),
-		green: hexadecimalParaDecimal(green),
-		blue: hexadecimalParaDecimal(blue),
-	};
+  return {
+    red: hexadecimalParaDecimal(red),
+    green: hexadecimalParaDecimal(green),
+    blue: hexadecimalParaDecimal(blue),
+  }
 }
 
-console.log(rgbParaHexadecimal(90, 4.9, 134));
+console.log(hexadecimalParaDecimal('0F'))
--- src/tests/hexrgb.test.js
+++ src/tests/hexrgb.test.js
@@ -1,7 +1,9 @@
-import { assert, expect } from 'chai';
-import { decimalParaHexadecimal, hexadecimalParaDecimal, rgbParaHexadecimal } from '../index.js';
-
-
+import { assert, expect } from 'chai'
+import {
+  decimalParaHexadecimal,
+  hexadecimalParaDecimal,
+  rgbParaHexadecimal,
+} from '../index.js'
 
 // hexToDec
 // verificando se está retornando o hexadecimal correto
@@ -16,35 +18,34 @@ import { decimalParaHexadecimal, hexadecimalParaDecimal, rgbParaHexadecimal } fr
 // caso tenha um valor incorreto, retornar objecto com prop erro: string com o erro
 
 describe('Testando conversão de decimal para hexadecimal', () => {
-
   it('verificando se está retornando o decimal correto', () => {
-    assert.equal(decimalParaHexadecimal(10), '0a');
-    assert.equal(decimalParaHexadecimal(220), 'dc');
-    assert.equal(decimalParaHexadecimal(16), '10');
-    assert.equal(decimalParaHexadecimal(32), '20');
-    assert.equal(decimalParaHexadecimal(171), 'ab');
-    assert.equal(decimalParaHexadecimal(157), '9d');
-    assert.equal(decimalParaHexadecimal(48), '30');
-    assert.equal(decimalParaHexadecimal(160), 'a0');
-    assert.equal(decimalParaHexadecimal(192), 'c0');
-  });
+    assert.equal(decimalParaHexadecimal(10), '0a')
+    assert.equal(decimalParaHexadecimal(220), 'dc')
+    assert.equal(decimalParaHexadecimal(16), '10')
+    assert.equal(decimalParaHexadecimal(32), '20')
+    assert.equal(decimalParaHexadecimal(171), 'ab')
+    assert.equal(decimalParaHexadecimal(157), '9d')
+    assert.equal(decimalParaHexadecimal(48), '30')
+    assert.equal(decimalParaHexadecimal(160), 'a0')
+    assert.equal(decimalParaHexadecimal(192), 'c0')
+  })
 
   it('se valor retornado é uma string', () => {
-    expect(decimalParaHexadecimal(10)).to.be.a('string');
-    expect(decimalParaHexadecimal(220)).to.be.a('string');
-    expect(decimalParaHexadecimal(171)).to.be.a('string');
-    expect(decimalParaHexadecimal(157)).to.be.a('string');
+    expect(decimalParaHexadecimal(10)).to.be.a('string')
+    expect(decimalParaHexadecimal(220)).to.be.a('string')
+    expect(decimalParaHexadecimal(171)).to.be.a('string')
+    expect(decimalParaHexadecimal(157)).to.be.a('string')
   })
 
   it('valor passado tem que ser um válido ', () => {
-    assert.equal(decimalParaHexadecimal(4.2), 'Isso não é um valor válido');
-    assert.equal(decimalParaHexadecimal('10'), 'Isso não é um valor válido');
-    assert.equal(decimalParaHexadecimal(-220), 'Isso não é um valor válido');
-    assert.equal(decimalParaHexadecimal('16'), 'Isso não é um valor válido');
-    assert.equal(decimalParaHexadecimal(true), 'Isso não é um valor válido');
-    assert.equal(decimalParaHexadecimal({}), 'Isso não é um valor válido');
-    assert.equal(decimalParaHexadecimal(['10']), 'Isso não é um valor válido');
-    assert.equal(decimalParaHexadecimal('*9'), 'Isso não é um valor válido');
+    assert.equal(decimalParaHexadecimal(4.2), 'Isso não é um valor válido')
+    assert.equal(decimalParaHexadecimal('10'), 'Isso não é um valor válido')
+    assert.equal(decimalParaHexadecimal(-220), 'Isso não é um valor válido')
+    assert.equal(decimalParaHexadecimal('16'), 'Isso não é um valor válido')
+    assert.equal(decimalParaHexadecimal(true), 'Isso não é um valor válido')
+    assert.equal(decimalParaHexadecimal({}), 'Isso não é um valor válido')
+    assert.equal(decimalParaHexadecimal(['10']), 'Isso não é um valor válido')
+    assert.equal(decimalParaHexadecimal('*9'), 'Isso não é um valor válido')
   })
 
   it('verificar se o tamanho da string retornada é de 2 dígitos', () => {
@@ -57,31 +58,78 @@ describe('Testando conversão de decimal para hexadecimal', () => {
     expect(decimalParaHexadecimal(157)).to.have.lengthOf(2)
     expect(decimalParaHexadecimal(77)).to.have.lengthOf(2)
   })
-});
+})
 
 describe('testando conversão rgb para hexadecimal', () => {
-
   it('caso os valores sejam validos, tem que retornar um objeto', () => {
     expect(rgbParaHexadecimal(157, 171, 122)).to.be.a('object')
     expect(rgbParaHexadecimal(16, 32, 192)).to.be.a('object')
     expect(rgbParaHexadecimal(255, 122, 100)).to.be.a('object')
     expect(rgbParaHexadecimal(33, 99, 134)).to.be.a('object')
-  });
+  })
   it('caso os valores forem validos, tem que conter red, green e blue', () => {
-    expect(rgbParaHexadecimal(157, 166, 180)).to.include.all.keys('red', 'green', 'blue');
-    expect(rgbParaHexadecimal(16, 32, 192)).to.include.all.keys('red', 'green', 'blue');
-    expect(rgbParaHexadecimal(255, 122, 100)).to.include.all.keys('red', 'green', 'blue');
-    expect(rgbParaHexadecimal(33, 99, 134)).to.include.all.keys('red', 'green', 'blue');
-  });
+    expect(rgbParaHexadecimal(157, 166, 180)).to.include.all.keys(
+      'red',
+      'green',
+      'blue',
+    )
+    expect(rgbParaHexadecimal(16, 32, 192)).to.include.all.keys(
+      'red',
+      'green',
+      'blue',
+    )
+    expect(rgbParaHexadecimal(255, 122, 100)).to.include.all.keys(
+      'red',
+      'green',
+      'blue',
+    )
+    expect(rgbParaHexadecimal(33, 99, 134)).to.include.all.keys(
+      'red',
+      'green',
+      'blue',
+    )
+  })
   it('caso um ou mais valores sejam incorretos, retornar objeto com o campo erro', () => {
-    expect(rgbParaHexadecimal(377, null, true)).to.have.property('erro');
-    expect(rgbParaHexadecimal(888, false, 192)).to.have.property('erro');
-    expect(rgbParaHexadecimal(455, 'string', 888)).to.have.property('erro');
-    expect(rgbParaHexadecimal(-1, 4.9, 134)).to.have.property('erro');
-    expect(rgbParaHexadecimal(90, 4.9, 134)).to.have.property('erro');
+    expect(rgbParaHexadecimal(377, null, true)).to.have.property('erro')
+    expect(rgbParaHexadecimal(888, false, 192)).to.have.property('erro')
+    expect(rgbParaHexadecimal(455, 'string', 888)).to.have.property('erro')
+    expect(rgbParaHexadecimal(-1, 4.9, 134)).to.have.property('erro')
+    expect(rgbParaHexadecimal(90, 4.9, 134)).to.have.property('erro')
+  })
+})
 
-  });
-});
+// hexToDec
+describe('testando conversão hexadecimal para decimal', () => {
+  it('verificando se o valor retornado está correto', () => {
+    assert.equal(hexadecimalParaDecimal('0a'), '10')
+    assert.equal(hexadecimalParaDecimal('dc'), '220')
+    assert.equal(hexadecimalParaDecimal('10'), '16')
+    assert.equal(hexadecimalParaDecimal('20'), '32')
+    assert.equal(hexadecimalParaDecimal('ab'), '171')
+    assert.equal(hexadecimalParaDecimal('9d'), '157')
+    assert.equal(hexadecimalParaDecimal('30'), '48')
+    assert.equal(hexadecimalParaDecimal('a0'), '160')
+    assert.equal(hexadecimalParaDecimal('c0'), '192')
+  })
 
+  it('verificando se o valor retornado é uma string', () => {
+    assert.equal(hexadecimalParaDecimal(4.2), 'Isso não é um valor válido')
+    assert.equal(hexadecimalParaDecimal('true'), 'Isso não é um valor válido')
+    assert.equal(hexadecimalParaDecimal(-220), 'Isso não é um valor válido')
+    assert.equal(hexadecimalParaDecimal(false), 'Isso não é um valor válido')
+    assert.equal(hexadecimalParaDecimal(true), 'Isso não é um valor válido')
+    assert.equal(hexadecimalParaDecimal({}), 'Isso não é um valor válido')
+    assert.equal(hexadecimalParaDecimal(['10']), 'Isso não é um valor válido')
+    assert.equal(hexadecimalParaDecimal('*9'), 'Isso não é um valor válido')
+  })
 
-
+  it('verificando se o valor hexadecimal é uma string', () => {
+    expect(hexadecimalParaDecimal('10')).to.be.a('string')
+    expect(hexadecimalParaDecimal('220')).to.be.a('string')
+    expect(hexadecimalParaDecimal('171')).to.be.a('string')
+    expect(hexadecimalParaDecimal('157')).to.be.a('string')
+    expect(hexadecimalParaDecimal('16')).to.be.a('string')
+    expect(hexadecimalParaDecimal('256')).to.be.a('string')
+    expect(hexadecimalParaDecimal('-1')).to.be.a('string')
+  })
+})
