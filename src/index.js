d71875ba2ee81fc37eb396f70415116185d8d122
ladeirakelwin <ladeirakelwin@gmail.com>
2022-05-21 14:52:37 -0300

    função renomeando funções e exportando

--- src/index.js
+++ src/index.js
@@ -16,58 +16,68 @@ const mapToHex = {
 	15: 'f',
 };
 
-function decToHex(dec) {
-  const result = dec / 16;
+function decimalParaHexadecimal(dec) {
+	const result = dec / 16;
 
-  const isInt =  (!String(result).includes('.'))
+	const ehInteiro = !String(result).includes('.');
 
-  if (isInt) {
-    const stringDec = String(result)
-    if (result < 10) return 0 + stringDec
-    return mapToHex[stringDec]
-  }
+	if (ehInteiro) {
+		const stringDec = String(result);
+		if (result < 10) return 0 + stringDec;
+		return mapToHex[stringDec];
+	}
 
-  const [inteiro, otherdecimal] =  String(result).split('.')
-  const decimal = Number('0.'+otherdecimal)
+	const [inteiro, otherdecimal] = String(result).split('.');
+	const decimal = Number('0.' + otherdecimal);
 
-  const primeiroHex = Number(inteiro) < 10 ? inteiro : mapToHex[inteiro];
-  const preSegundoHex = String(decimal * 16)
-  const segundoHex = Number(preSegundoHex) < 10 ? preSegundoHex : mapToHex[preSegundoHex]
+	const primeiroHex = Number(inteiro) < 10 ? inteiro : mapToHex[inteiro];
+	const preSegundoHex = String(decimal * 16);
+	const segundoHex = Number(preSegundoHex) < 10 ? preSegundoHex : mapToHex[preSegundoHex];
 
-  return primeiroHex + segundoHex
-
+	return primeiroHex + segundoHex;
 }
 
-function rgbToHex(red, green, blue) {
-  return {
-    red: decToHex(red),
-    green: decToHex(green),
-    blue: decToHex(blue)
-  } 
+function rgbParaHexadecimal(red, green, blue) {
+	return {
+		red: decimalParaHexadecimal(red),
+		green: decimalParaHexadecimal(green),
+		blue: decimalParaHexadecimal(blue),
+	};
 }
 
-function hexToDec(hexInput) {
-  const valoresInvalidos = ['', undefined, null]
-  if(hexInput.length > 2 || String(hexInput).includes('-') || valoresInvalidos.includes(hexInput)) return 'Isso não é um valor válido'
-  const hex =  String(hexInput).toLowerCase()
-  const letras = ['a', 'b', 'c', 'd', 'e', 'f']
-  const [primeiroHex, segundoHex] = String(hex)
-  const ehUmaLetra = (value) => letras.includes(value) 
-  const prePrimeiroHex = ehUmaLetra(primeiroHex) ? Number(mapToDec[primeiroHex]) : Number(primeiroHex)
-  const primeiroDec =  prePrimeiroHex * 16
+function hexadecimalParaDecimal(hexadecimal) {
+	const valoresInvalidos = ['', undefined, null];
+	if (
+		hexadecimal.length > 2 ||
+		String(hexadecimal).includes('-') ||
+		valoresInvalidos.includes(hexadecimal)
+	)
+		return 'Isso não é um valor válido';
+	const hex = String(hexadecimal).toLowerCase();
+	const letras = ['a', 'b', 'c', 'd', 'e', 'f'];
+	const [primeiroHex, segundoHex] = String(hex);
+	const ehUmaLetra = (value) => letras.includes(value);
+	const prePrimeiroHex = ehUmaLetra(primeiroHex)
+		? Number(mapToDec[primeiroHex])
+		: Number(primeiroHex);
+	const primeiroDec = prePrimeiroHex * 16;
 
-  const segundoDec = ehUmaLetra(segundoHex) ? Number(mapToDec[segundoHex]) : Number(segundoHex)
+	const segundoDec = ehUmaLetra(segundoHex) ? Number(mapToDec[segundoHex]) : Number(segundoHex);
 
-  return String(primeiroDec + segundoDec)
-
+	return String(primeiroDec + segundoDec);
 }
 
-function hexToRGB(red, green, blue) {
-  return  {
-    red: hexToDec(red),
-    green: hexToDec(green),
-    blue: hexToDec(blue)
-  } 
+function hexadecimalParaRGB(red, green, blue) {
+	return {
+		red: hexadecimalParaDecimal(red),
+		green: hexadecimalParaDecimal(green),
+		blue: hexadecimalParaDecimal(blue),
+	};
 }
 
-console.log(rgbToHex(255,10, 40))
+module.exports = {
+	hexadecimalParaDecimal,
+	hexadecimalParaRGB,
+	decimalParaHexadecimal,
+	rgbParaHexadecimal,
+};
